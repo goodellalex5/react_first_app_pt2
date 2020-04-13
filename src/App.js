08c11d77de54c997d2877dca1f74fd4cd766e508
goodellalex5 <goodellalex5@gmail.com>
2020-04-13 10:59:42 -0700

    Finished through step 4
    Code fails to compile, it throws an error on Form.js
    Line 19:10 parsing error: unexpected token, expected ";" could not find fix

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
 import React, { Component } from 'react'
 import Table from './Table'
+import Form from './Form';
+
 
 class App extends Component {
-  render() {
-    const characters = [
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-    ]
-
-    return (
-      <div className="container">
-        <Table characterData={characters}/>
-      </div>
-    )
+  state = {
+    characters: [],
   }
+removeCharacter = index => {
+	const {characters } = this.state
+	
+	this.setState({
+		characters: characters.filter((character, i) => {
+		return i !== index}),
+	})
+	}
+	
+	render(){
+		const {characters } = this.state
+		
+		return(
+		<div className = "container">
+		<Table characterData = {characters} removeCharacter = {this.removeCharacter} />
+		<Form />
+		</div>
+		)
+	}
 }
 
-export default App
+export default App;
