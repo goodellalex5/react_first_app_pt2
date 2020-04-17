9060ea1fc39265479f4f0a8279b8a5caa73fc92b
goodellalex5 <goodellalex5@gmail.com>
2020-04-17 11:38:01 -0700

    Finished steps through 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Table from './Table'
 import Form from './Form';
+import axios from 'axios';
 
 
 class App extends Component {
@@ -16,9 +17,13 @@ removeCharacter = index => {
 	})
 	}
 
-	handleSubmit = person => {
-	this.setState({characters: [...this.state.characters,person]})
-	}
+handleSubmit = character => {
+   this.makePostCall(character).then( callResult => {
+      if (callResult === true) {
+         this.setState({ characters: [...this.state.characters, character] });
+      }
+   });
+ }
 	render(){
 		const {characters } = this.state;
 		
@@ -29,6 +34,28 @@ removeCharacter = index => {
 		</div>
 		)
 	}
+componentDidMount() {
+   axios.get('http://localhost:5000/users')
+    .then(res => {
+      const characters = res.data.users_list;
+      this.setState({ characters });
+    })
+    .catch(function (error) {
+      //Not handling the error. Just logging into the console.
+      console.log(error);
+    });
+}
+makePostCall(character){
+   return axios.post('http://localhost:5000/users', character)
+    .then(function (response) {
+      console.log(response);
+      return (response.status === 200);
+    })
+    .catch(function (error) {
+      console.log(error);
+      return false;
+    });
+ }
 }
 
 export default App;
